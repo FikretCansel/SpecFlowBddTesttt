a9cb11448f048324c240deb4856f69dce28f4f08
Fikret Cansel <fcansel19@posta.pau.edu.tr>
2021-08-03 14:56:40 +0300

    Add project files.

---
+++ SpecFlowCalculator2/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowCalculator2
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+    }
+}
---
+++ SpecFlowCalculator2/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowCalculator2
+{
+    public class ProductManager
+    {
+        List<Product> products;
+        public ProductManager()
+        {
+            products = new List<Product>();
+        }
+        public void AddToCart(Product product)
+        {
+            products.Add(product);
+        }
+        public void RemoveFromCart(Product product)
+        {
+            products.Remove(product);
+        }
+
+    }
+}
